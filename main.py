6b4913407f531cb31a4fe430c2b862f14a13f8ae
Roman Ismagilov <pomis172@gmail.com>
2018-11-13 19:56:24 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
 import sys
-
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
@@ -19,7 +18,6 @@ def get_words(txt):
 
 
 def parse_sentence_words(input_file_names):
-    """Returns a list of a list of words. Each sublist is a sentence."""
     sentence_words = []
     for file_name in input_file_names:
         for line in open(file_name):
@@ -32,16 +30,21 @@ def parse_sentence_words(input_file_names):
     return sentence_words
 
 
-# You would see five .txt files after unzip 'text.zip'
 input_file_names = ["text/001ssb.txt", "text/002ssb.txt", "text/003ssb.txt",
-                    "text/004ssb.txt", "text/005ssb.txt"]
+                    "text/004ssb.txt", "text/005ssb.txt", "text/article1.txt",
+                    "text/article2.txt", "text/article3.txt", "text/article4.txt",
+                    "text/article5.txt", "text/article6.txt", "text/article7.txt",
+                    "text/article8.txt"]
 GOT_SENTENCE_WORDS = parse_sentence_words(input_file_names)
 
-# size: the dimensionality of the embedding vectors.
-# window: the maximum distance between the current and predicted word within a sentence.
-model = Word2Vec(GOT_SENTENCE_WORDS, size=128, window=3, min_count=5, workers=4)
+model = Word2Vec(GOT_SENTENCE_WORDS, size=64, window=3, min_count=5, workers=4)
 model.wv.save_word2vec_format("got_word2vec.txt", binary=False)
 
-i = model.wv.index2word.index("stark")
-d = model.wv.index2word.index("king")
-print model.wv.vectors[i] - model.wv.vectors[d]
+parent = model.wv.index2word.index("riverrun")
+son = model.wv.index2word.index("riverlands")
+name_rel = model.wv.vectors[parent] - model.wv.vectors[son]
+parent2 = model.wv.index2word.index("winterfell")
+test = model.wv.vectors[parent2] - name_rel
+print test
+res = model.wv.similar_by_vector(test, 10)
+print res
